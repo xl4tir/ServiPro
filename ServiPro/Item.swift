a560a7eb72d7c35d401c363b5261c588dfc23e00
xl4tir <52316874+xl4tir@users.noreply.github.com>
2024-04-12 18:29:03 +0300

    Initial Commit

---
+++ ServiPro/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  ServiPro
+//
+//  Created by Владислав Опиченік on 12.04.2024.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ ServiPro/ServiProApp.swift
@@ -0,0 +1,32 @@
+//
+//  ServiProApp.swift
+//  ServiPro
+//
+//  Created by Владислав Опиченік on 12.04.2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ServiProApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
